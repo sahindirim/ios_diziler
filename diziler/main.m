00d2b093d31bfd7a7db665fcd42bd6af7fe05ca6
Şahin Dirim <sahindirim@Sahin-MacBook-Pro.local>
2014-09-27 01:18:03 +0300

    Initial Commit

---
+++ diziler/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  diziler
+//
+//  Created by Şahin Dirim on 27.09.2014.
+//  Copyright (c) 2014 Şahin Dirim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
